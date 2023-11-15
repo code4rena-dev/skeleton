ca9ad44e5f08757e00eebaf3b0945e5dd993317f
nlf <nlf@nlf.sh>
2023-11-15 11:28:29 -0800

    feat: update to code-skeleton@2

--- lib/mustache.ts
+++ lib/mustache.ts
@@ -1,18 +1,14 @@
-import {
-  Generator,
-  GeneratorResults,
-  GeneratorOptions
-} from "code-skeleton/lib/generators/abstract";
-import { dirname } from "node:path";
-import { writeFile, mkdir, readFile } from "node:fs/promises";
+import { Generator } from "code-skeleton/lib/generators/abstract";
+import { readFile } from "node:fs/promises";
 import Mustache from "mustache";
 Mustache.tags = [ "<%", "%>" ];
 
-interface MustacheGeneratorOptions extends GeneratorOptions {
+interface MustacheGeneratorOptions {
   sourcePath: string;
+  variables: unknown;
 }
 
-class MustacheGenerator extends Generator {
+class MustacheGenerator extends Generator<MustacheGeneratorOptions> {
   declare options: MustacheGeneratorOptions;
 
   constructor (options: MustacheGeneratorOptions) {
@@ -23,47 +19,14 @@ class MustacheGenerator extends Generator {
     }
   }
 
-  async apply(targetPath: string): Promise<GeneratorResults> {
-    await mkdir(dirname(targetPath), { recursive: true });
-    try {
-      const source = await readFile(this.options.sourcePath);
-
-      const rendered = Mustache.render(source.toString(), this.options);
-      await writeFile(targetPath, rendered);
-      return this.pass();
-    } catch (err) {
-      const { code, message } = err as { code?: string; message: string };
-      // istanbul ignore next - no need to test message fallback
-      return this.fail(code ?? message);
-    }
-  }
-  async verify(targetPath: string): Promise<GeneratorResults> {
-    let actual;
-    try {
-      actual = await readFile(targetPath);
-    } catch (err) {
-      const { code, message } = err as { code?: string; message: string };
-      // istanbul ignore next - no need to test passthrough throws
-      if (code !== "ENOENT") {
-        return this.fail(code ?? message);
-      }
-
-      return this.fail("file missing");
-    }
-
+  async generate () {
     const source = await readFile(this.options.sourcePath);
-    const expected = Buffer.from(Mustache.render(source.toString(), this.options));
-    if (actual.compare(expected) === 0) {
-      return this.pass();
-    }
 
-    return this.fail("contents do not match");
+    const rendered = Mustache.render(source.toString(), this.options.variables);
+    return rendered;
   }
 }
 
-export function mustache (sourcePath: string, options: GeneratorOptions = {}) {
-  return new MustacheGenerator({
-    ...options,
-    sourcePath
-  });
+export function mustache (options: MustacheGeneratorOptions) {
+  return new MustacheGenerator(options);
 }
